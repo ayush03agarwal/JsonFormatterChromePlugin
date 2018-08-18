457d68c8bdaf349542c82f9f88731bbbf44f91e5
ayush03agarwal <ayush03agarwal@gmail.com>
2018-08-18 12:05:41 +0530

    Added escape and unescape functionality for string

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,15 +1,20 @@
-
 document.getElementById("callPretty").addEventListener("click", prettyPrint);
 document.getElementById("newTab").addEventListener("click", newTab);
 document.getElementById("callRaw").addEventListener("click", rawPrint);
 document.getElementById("callCopyMe").addEventListener("click", copyMe);
-window.onload = function(){addLinesInTextarea();};
+document.getElementById("jsonStringEscape").addEventListener("click", escapeJson);
+document.getElementById("jsonStringUnescape").addEventListener("click", unescapeJson);
+
+
+window.onload = function () {
+    addLinesInTextarea();
+};
 
 JSON._parse = JSON.parse
 JSON.parse = function (json) {
     try {
         return JSON._parse(json)
-    } catch(e) {
+    } catch (e) {
         jsonlint.parse(json)
     }
 }
@@ -25,6 +30,48 @@ function prettyPrint() {
         $("#infoArea").html(exc + '');
     }
 }
+
+function escapeJson() {
+    try {
+        var unescaped = document.getElementById('dumpArea').value;
+        var myJSONString = JSON.stringify(unescaped);
+        myJSONString = myJSONString.substring(1, myJSONString.length - 1);
+        document.getElementById('dumpArea').value = myJSONString;
+        $("#infoArea").html("Escaped text");
+    } catch (exc) {
+        $("#infoArea").html(exc + '');
+    }
+}
+
+function unescapeJson() {
+    try {
+        var escaped = document.getElementById('dumpArea').value;
+        var myJSONString = escaped.unescapeSpecialChars();
+        document.getElementById('dumpArea').value = myJSONString;
+        $("#infoArea").html("Unescaped text");
+    } catch (exc) {
+        $("#infoArea").html(exc + '');
+    }
+}
+
+String.prototype.unescapeSpecialChars = function () {
+    var original = this
+    var replaced = this.replace(/\\\\/g, "\\")
+    if (original.toString() !== replaced.toString()) {
+        return this.replace(/\\\\/g, "\\")
+            .replace(/\\'/g, "'")
+            .replace(/\\"/g, '"');
+    } else {
+        return this.replace(/\\'/g, "'")
+            .replace(/\\"/g, '"')
+            .replace(/\\&/g, "\&")
+            .replace(/\\n/g, "\n")
+            .replace(/\\r/g, "\r")
+            .replace(/\\t/g, "\t")
+            .replace(/\\b/g, "\b")
+            .replace(/\\f/g, "\f");
+    }
+};
 
 function rawPrint() {
     var formatted = document.getElementById('dumpArea').value;
@@ -44,8 +91,8 @@ function copyMe() {
 }
 
 function newTab() {
-     chrome.tabs.create({url: chrome.extension.getURL('newTab.html#window')});
-     return false;
+    chrome.tabs.create({url: chrome.extension.getURL('newTab.html#window')});
+    return false;
 }
 
 function addLinesInTextarea() {
